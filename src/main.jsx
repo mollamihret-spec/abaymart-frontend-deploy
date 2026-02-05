01bc1a4ed7af7536394a2e5dae58421826d4f839
mollamihret-spec <molamihert@gmail.com>
2026-02-05 16:02:09 +0300

    updated main
    case update on main

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { DataProvider } from './Components/DataProvider/DataProvider.jsx';
-import { Reducer, initialState } from './Utility/rEDUCER.JSX';
+import { Reducer, initialState } from './Utility/Reducer.JSX';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
