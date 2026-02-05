2ea7ae6a84626ac22fb3e6e562b9ee65f9eb0850
mollamihret-spec <molamihert@gmail.com>
2026-02-05 21:11:32 +0300

    update axios
    update axios

--- src/API/axios.js
+++ src/API/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   // local url
   // baseURL: "http://127.0.0.1:5001/clone-f5c60/us-central1/api",
   
-  baseURL: "https://abaymart-api-deploy.onrender.com/",
+  baseURL: "https://abaymart-api-deploy.onrender.com",
 });
 
